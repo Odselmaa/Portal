4512743d4f610f19a2a5ccbb43e870628ee6ea44
Odselmaa <se11d002@gmail.com>
2018-05-29 20:59:40 +0300

    db changes

--- routes/connection.js
+++ routes/connection.js
@@ -7,7 +7,7 @@ var session = express_session({
   secret: 'a4f8071f-c873-4447-8ee22d-dddd1',
   name: 'nameOfCookie',
   store: new MongoStore({
-    url: 'mongodb://admin_remine:WinniePooh8@remineme-shard-00-00-h4vdb.mongodb.net:27017,remineme-shard-00-01-h4vdb.mongodb.net:27017,remineme-shard-00-02-h4vdb.mongodb.net:27017/Portal?ssl=true&replicaSet=RemineMe-shard-0&authSource=admin',
+    url:   'mongodb://admin_remine:WinniePooh8@cluster0-shard-00-00-h4vdb.mongodb.net:27017,cluster0-shard-00-01-h4vdb.mongodb.net:27017,cluster0-shard-00-02-h4vdb.mongodb.net:27017/Portal?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin',
     autoRemove: 'native',
     collection: 'session',
   }),
