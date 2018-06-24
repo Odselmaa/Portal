5e6485279ec3df5673a3ceca306cfc23d262b3a4
Odselmaa <se11d002@gmail.com>
2018-06-25 01:30:59 +0300

    locale review

--- middleware.js
+++ middleware.js
@@ -49,8 +49,8 @@ module.exports = {
                    }
 
                }).catch((err)=>{
-                   console.log(err)
-                res.send(err)
+                    console.log(err)
+                    res.send(err)
                })
             }
 
